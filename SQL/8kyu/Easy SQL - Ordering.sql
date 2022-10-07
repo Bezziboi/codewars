3af983c2f514eef1f585b363c237fc0d917f7498
Bezziboi <bezirgen.09142000@mail.ru>
2022-10-07 17:56:27 +0300

    .

---
+++ SQL/8kyu/Easy SQL - Ordering.sql
@@ -0,0 +1,11 @@
+-- Your task is to sort the information in the provided table 'companies' by number of employees (high to low).
+-- Returned table should be in the same format as provided:
+
+-- companies table schema
+
+-- id
+-- ceo
+-- motto
+-- employees
+
+SELECT * FROM companies ORDER BY employees DESC;
